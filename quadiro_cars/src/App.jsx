97c9242f493c9c5ee9d0a3dd2c58cd1eac01dc8f
abdul8840 <abdul14941naseer@gmail.com>
2024-08-05 11:59:34 +0530

    create footer component ui

--- quadiro_cars/src/App.jsx
+++ quadiro_cars/src/App.jsx
@@ -6,6 +6,7 @@ import SignUp from './pages/SignUp';
 import Dashboard from './pages/Dashboard';
 import Header from './components/Header';
 import 'flowbite/dist/flowbite.css';
+import Footer from './components/Footer';
 
 
 const App = () => {
@@ -18,6 +19,7 @@ const App = () => {
         <Route path='/sign-up' element={<SignUp />} />;
         <Route path='/dashboard' element={<Dashboard />} />;
       </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
