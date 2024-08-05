aaff6eabe28e8d38bc0782baf89f4b8ae98eec56
abdul8840 <abdul14941naseer@gmail.com>
2024-08-05 10:41:01 +0530

    create ui of header component

--- quadiro_cars/src/App.jsx
+++ quadiro_cars/src/App.jsx
@@ -4,10 +4,14 @@ import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Dashboard from './pages/Dashboard';
+import Header from './components/Header';
+import 'flowbite/dist/flowbite.css';
+
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />;
         <Route path='/sign-in' element={<SignIn />} />;
