f1368ff84c02265dba7788ff4c814321fcb9b21c
abdul8840 <abdul14941naseer@gmail.com>
2024-08-05 10:22:33 +0530

    create pages and routes

--- quadiro_cars/src/App.jsx
+++ quadiro_cars/src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Dashboard from './pages/Dashboard';
 
 const App = () => {
   return (
-    <div className='text-2xl text-red-500'>
-      App
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />;
+        <Route path='/sign-in' element={<SignIn />} />;
+        <Route path='/sign-up' element={<SignUp />} />;
+        <Route path='/dashboard' element={<Dashboard />} />;
+      </Routes>
+    </BrowserRouter>
   )
 }
 
