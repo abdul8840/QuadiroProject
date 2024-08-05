b653ae9b97974818ee78eac67ecd8135389d6b5f
abdul8840 <abdul14941naseer@gmail.com>
2024-08-05 18:23:40 +0530

    add pre installlation

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import authRoutes from './routes/auth.route.js';
 import userRoutes from './routes/user.route.js';
 import postRoutes from './routes/post.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 dotenv.config();
 
@@ -13,6 +14,8 @@ mongoose.connect(process.env.MONGO).then(() => {
 }).catch(err => {
     console.log(err)
 })
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -26,6 +29,12 @@ app.listen(3000, () => {
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
