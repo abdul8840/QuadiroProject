318fe1b2db2dc9d18b439f315f474fa9cd60532c
abdul8840 <abdul14941naseer@gmail.com>
2024-08-05 18:38:20 +0530

    update pre installation

--- api/index.js
+++ api/index.js
@@ -30,10 +30,10 @@ app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(express.static(path.join(__dirname, '/quadiro_cars/dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'quadiro_cars', 'dist', 'index.html'));
 });
 
 app.use((err, req, res, next) => {
